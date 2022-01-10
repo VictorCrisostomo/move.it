cdd2dc65be556268a88554fa5e15b283e5a54dba
Victor Crisostomo Olivieira <victorcrisostomoliveira@gmail.com>
2022-01-10 13:53:44 -0300

    Update Profile.tsx
    edit profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -8,7 +8,7 @@ export function Profile() {
 
     return (
         <div className= {styles.profileContainer}>
-            <img src="https://instagram.funa1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/271200141_450402556555413_6801541137254318147_n.jpg?_nc_ht=instagram.funa1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=3WxV-x6NwB4AX_734QN&edm=ALbqBD0BAAAA&ccb=7-4&oh=00_AT_3YwA9-GkamiqBbXqMR2fsy9L6HaLoOFX3ZjjcaAMDFQ&oe=61E2B5F0&_nc_sid=9a90d6" alt="Gabi Lana"/>
+            <img src="<img alt="https://instagram.funa1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/271200141_450402556555413_6801541137254318147_n.jpg?_nc_ht=instagram.funa1-1.fna.fbcdn.net&amp;_nc_cat=109&amp;_nc_ohc=3WxV-x6NwB4AX_734QN&amp;edm=ALbqBD0BAAAA&amp;ccb=7-4&amp;oh=00_AT_3YwA9-GkamiqBbXqMR2fsy9L6HaLoOFX3ZjjcaAMDFQ&amp;oe=61E2B5F0&amp;_nc_sid=9a90d6">" alt="Gabi Lana"/>
             <div>
                 <strong>Gabi Alana</strong>
                 <p>
